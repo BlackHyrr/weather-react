fdb43534ac1c470b428ee6de4a2c66458dc535b7
jboude <boudejerome@gmail.com>
2024-03-14 14:59:45 +0100

    minor style improvement

--- src/App.jsx
+++ src/App.jsx
@@ -27,20 +27,18 @@ function App() {
             <h1>Weather API</h1>
             {console.log(weatherData)}
             <InputLocation />
-            <div className='weather-display'>
-                {weatherData && (
-                    <div>
-                        <h2>Location: {weatherData.name}</h2>
-                        <p>Temperature: {weatherData.main.temp ? weatherData.main.temp + '°C' : 'Temperature Unknown'}</p>
-                        <p>Humidity: {weatherData.main.humidity ? weatherData.main.humidity + '%' : 'Humidity Unknown'}</p>
-                        <p>Description: {weatherData.weather[0].description ? weatherData.weather[0].description : 'No description'}</p>
-                        <p>
-                            Wind: { 
-                            weatherData.wind ? Math.floor(parseFloat(weatherData.wind.speed * 3.6) * 10 / 10) + 'km/h' : 'No data available'} - {weatherData.wind && getWindDirection(weatherData.wind.deg)}
-                        </p>
-                    </div>
-                )}
-            </div>
+            {weatherData && (
+                <div className='weather-display'>
+                    <h2>Location: {weatherData.name}</h2>
+                    <p>Temperature: {weatherData.main.temp ? weatherData.main.temp + '°C' : 'Temperature Unknown'}</p>
+                    <p>Humidity: {weatherData.main.humidity ? weatherData.main.humidity + '%' : 'Humidity Unknown'}</p>
+                    <p>Description: {weatherData.weather[0].description ? weatherData.weather[0].description : 'No description'}</p>
+                    <p>
+                        Wind: { 
+                        weatherData.wind ? Math.floor(parseFloat(weatherData.wind.speed * 3.6) * 10 / 10) + 'km/h' : 'No data available'} - {weatherData.wind && getWindDirection(weatherData.wind.deg)}
+                    </p>
+                </div>
+            )}
         </>
     );
 }
